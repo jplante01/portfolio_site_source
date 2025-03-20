b535a9f3f599dfdac9ef033db19eba1f87f5e724
Jeffrey P <jeffp.web@gmail.com>
2025-03-20 12:20:09 -0700

    Update the copywright statement

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -78,7 +78,7 @@ const Footer = () => {
           </div>
         </div>
         <div className="border-t border-gray-700 mt-8 pt-8 text-center text-gray-300">
-          <p>&copy; 2025 Your Name. All rights reserved.</p>
+          <p>&copy; 2025 J.P. All rights reserved.</p>
         </div>
       </div>
     </footer>
