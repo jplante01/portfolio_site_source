1d3b2107dbac8dcb89b2c126ddc1a89f6781bfb7
Jeffrey P <jeffp.web@gmail.com>
2025-03-21 14:19:35 -0700

    Add border

--- src/components/PortfolioCard.jsx
+++ src/components/PortfolioCard.jsx
@@ -1,7 +1,7 @@
 const PortfolioCard = ({ image, title, description, tags, link, github }) => {
   return (
     <div className="bg-gray-100 p-6 rounded-lg overflow-hidden shadow-lg hover:shadow-xl shadow-gray-800 transition-all duration-300 hover:scale-105 border border-gray-200">
-      <img src={image} alt={title} className="w-full h-48 object-cover mb-4 rounded-lg" />
+      <img src={image} alt={title} className="w-full h-48 object-cover mb-4 rounded-lg border border-2" />
       <div className="">
         <h3 className="text-xl font-bold mb-2">{title}</h3>
         <p className="text-gray-600 mb-4">{description}</p>
