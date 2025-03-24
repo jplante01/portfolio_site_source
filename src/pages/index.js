b42881fb333faf0c1b96b5e51569b3a3837e17df
Jeffrey P <jeffp.web@gmail.com>
2025-03-24 11:37:15 -0700

    Change the background

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,7 +11,7 @@ import Footer from '../components/Footer';
 // Main App Component
 const Home = ({ portfolioItems = [] }) => {
   return (
-    <div className="bg-gradient-to-br from-purple-700 via-purple-900 to-gray-900 flex flex-col min-h-screen">
+    <div className="background-gradient flex flex-col min-h-screen">
       <Header />
       <HeroSection />
       {portfolioItems && <PortfolioGrid portfolioItems={portfolioItems} />}
