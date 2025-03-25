c920150ea8d3dd2dc25f28f077b2b5b59c700c3b
Jeffrey P <jeffp.web@gmail.com>
2025-03-25 13:25:16 -0700

    Remove shadow

--- src/components/ComingSoonCard.jsx
+++ src/components/ComingSoonCard.jsx
@@ -1,6 +1,6 @@
 const ComingSoonCard = () => {
   return (
-    <div className="bg-transparent p-6 rounded-lg overflow-hidden shadow-lg hover:shadow-xl shadow-gray-400 transition-all duration-300 hover:scale-105 border border-gray-200">
+    <div className="bg-transparent p-6 rounded-lg overflow-hidden transition-all duration-300 hover:scale-105 border border-gray-200">
       <p className="text-gray-50 text-xl font-bold text-lexend">More coming soon...</p>
     </div>
   );
