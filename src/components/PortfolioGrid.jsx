821c86baff7e79d020192da0c38e302ecf7e1fd2
Jeffrey P <jeffp.web@gmail.com>
2025-03-25 09:40:49 -0700

    Remove Grid span

--- src/components/PortfolioGrid.jsx
+++ src/components/PortfolioGrid.jsx
@@ -5,7 +5,6 @@ const PortfolioGrid = ({ portfolioItems }) => {
     <section className="pt-16 pb-64 px-6">
       <div className="container mx-auto">
         {/* <h2 className="text-3xl font-bold mb-12 text-center text-gray-50 font-lexend">My Portfolio</h2> */}
-        <h2 className="portfolio-span font-lexend text-center text-gray-50 pb-6">MY PORTFOLIO</h2>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
           {portfolioItems.map((item) => (
             <PortfolioCard
